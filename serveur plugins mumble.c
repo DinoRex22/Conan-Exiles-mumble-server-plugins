0142b470c8f7f6748175672e642b70f5d6acb64b
Dino_Rex <62900420+DinoRex22@users.noreply.github.com>
2024-12-02 14:30:27 -0500

    Update serveur plugins mumble.c

--- serveur plugins mumble.c
+++ serveur plugins mumble.c
@@ -1,3 +1,14 @@
+MIT License
+
+Copyright (c) 2024 Dino_Rex
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is provided to do so, subject to the following conditions:
+
+1. Plugin creators must credit the author (Dino_Rex) and include a link to the Discord https://discord.gg/tFBbQzmDaZ in both the source code and the description of the compiled Mumble plugin.
+2. All usage of this code must remain open source. A link to the open-source project must be shared on the Discord https://discord.gg/tFBbQzmDaZ in the "open-source" channel.
+3. Redistribution, modification, or use of the code in any way is allowed only if these conditions are met.
+4. This code may not be used in any proprietary, non-open-source software.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
